acde860c0a119122610cc0c4b1521aec29e2a57c
Soni Anant Rajeshkumar <137305850+Theanantsoni@users.noreply.github.com>
2024-08-07 13:54:21 +0530

    Add files via upload

---
+++ 23. RESET PASSWORD & EMAIL VERIFICATION/Admin/connection.php
@@ -0,0 +1,11 @@
+<?php
+    $conn = mysqli_connect('localhost:1111','root','','dbform') or die("Connection failed");
+   /* if($conn)
+    {
+        echo "successful connection";
+    }
+    else
+    {
+        echo "Error";
+    } */
+?>
