1974c6f66a816fb0455672535bd8c96a8e8f3990
Soni Anant Rajeshkumar <137305850+Theanantsoni@users.noreply.github.com>
2024-08-07 15:15:03 +0530

    Update DIS_REP_ANS.php

--- 8. DISCOUNT_REPORT/DIS_REP_ANS.php
+++ 8. DISCOUNT_REPORT/DIS_REP_ANS.php
@@ -1,10 +1,97 @@
-
-<?php//------------------------------------FIRST DISCOUNT CODING---------------------------------------?>
+<?php//------------------------------------No DISCOUNT CODING---------------------------------------?>
 
 <?php
 	if(isset($_POST['btnsubmit']))
 	{
 		$product_price=$_POST['txtpro'];
+		
+		if($product_price<20000 and $product_price<=40000)
+		{
+			$dis_first=$product_price*0;
+			$pay1=$product_price-$dis_first;
+
+?>
+
+<center>
+
+	<table border="5" width="50%" bordercolor="red";>
+
+		<tr>
+			<th>
+				<font>
+					Your Product Price Is : 
+				</font>
+			</th>
+
+			<td>
+				<font>
+					<?php
+						echo $product_price;
+					?>
+				</font>
+			</td>
+		</tr>
+
+		<tr>
+			<th>
+				<font>
+					Your Discount Is : 
+				</font>
+			</th>
+
+			<td>
+				<font>
+					0 %
+				</font>
+			</td>
+		</tr>
+
+
+		<tr>
+			<th>
+				<font>
+					Your Total Discount Amount Is : 
+				</font>
+			</th>
+
+			<td>
+				<font>
+					<?php
+						echo $dis_first;
+					?>
+				</font>
+			</td>
+		</tr>
+
+		<tr>
+			<th>
+				<font>
+					Your Have To Pay : 
+				</font>
+			</th>
+
+			<td>
+				<font>
+					<?php
+						echo $pay1 ." RS/- Only";
+					?>
+				</font>
+			</td>
+		</tr>
+	
+	</table>
+
+	<h3 style="color: red";>No Discount Found Less Then 20,000/- Rs/-</h3>
+
+</center>
+
+<?php
+		}
+?>
+
+<?php//------------------------------------FIRST DISCOUNT CODING---------------------------------------?>
+
+<?php
 		
 		if($product_price>=20000 and $product_price<=40000)
 		{
